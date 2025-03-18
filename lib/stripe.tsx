7a3393f8e9e8d8ebdc0f8451954ef754d97e4213
jlh425 <105890316+jlh425@users.noreply.github.com>
2025-03-18 06:49:47 -0600

    added stripe.tsx

---
+++ lib/stripe.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Stripe from 'stripe';
+
+// Initialize Stripe.js
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+
+initialize();
+
+// Fetch Checkout Session and retrieve the client secret
+async function initialize() {
+  const fetchClientSecret = async () => {
+    const response = await fetch("/create-checkout-session", {
+      method: "POST",
+    });
+    const { clientSecret } = await response.json();
+    return clientSecret;
+  };
+
+  // Initialize Checkout
+  const checkout = await stripe.initEmbeddedCheckout({
+    fetchClientSecret,
+  });
+
+  // Mount Checkout
+  checkout.mount('#checkout');
+}
