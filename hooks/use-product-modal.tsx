0849e719553dba906f2493adc3a712fe71edb476
jlh425 <105890316+jlh425@users.noreply.github.com>
2025-05-15 06:21:49 -0600

    fix: update editId type to string | number for consistency in useProductModalStore

--- hooks/use-product-modal.tsx
+++ hooks/use-product-modal.tsx
@@ -3,9 +3,9 @@ import { create } from 'zustand';
 interface useProductModalStore {
   isOpen: boolean;
   isEdit: boolean;
-  editId?: string;
+  editId?: string | number;
   onOpen: () => void;
-  onEdit: (id: integer) => void;
+  onEdit: (id: number) => void;
   onClose: () => void;
 }
 
@@ -14,6 +14,6 @@ export const useProductModal = create<useProductModalStore>((set) => ({
   isEdit: false,
   editId: undefined,
   onOpen: () => set({ isOpen: true }),
-  onEdit: (id: integer) => set({ isOpen: true, isEdit: true, editId: id }),
+  onEdit: (id: number) => set({ isOpen: true, isEdit: true, editId: id }),
   onClose: () => set({ isOpen: false, isEdit: false, editId: undefined }),
 }));
