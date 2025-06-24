7ea8c93599803902af722441c74988eafd3b8013
jlh425 <105890316+jlh425@users.noreply.github.com>
2025-06-24 05:51:12 -0600

    refactor: rename 'value' to 'description' in PATCH function for tool updates

--- app/api/[storeId]/tool/[toolId]/route.ts
+++ app/api/[storeId]/tool/[toolId]/route.ts
@@ -70,7 +70,7 @@ export async function PATCH(
 
     const body = await req.json();
 
-    const { name, value } = body;
+    const { name, description } = body;
 
     if (!userId) {
       return new NextResponse("Unauthenticated", { status: 403 });
@@ -80,8 +80,8 @@ export async function PATCH(
       return new NextResponse("Name is required", { status: 400 });
     }
 
-    if (!value) {
-      return new NextResponse("Value is required", { status: 400 });
+    if (!description) {
+      return new NextResponse("Description is required", { status: 400 });
     }
 
     if (!params.toolId) {
@@ -105,8 +105,7 @@ export async function PATCH(
       },
       data: {
         name,
-        value,
-      
+        description,
       },
     });
 
